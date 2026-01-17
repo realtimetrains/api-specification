f491eb9d59c7791340b6d5ea67255753320afc57
Tom Cairns <tom@swlines.co.uk>
2026-01-17 12:06:26 +0000

    Rework specification location

--- swagger-dist/swagger-initializer.js
+++ swagger-dist/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "specification/draft.yml",
+    url: "specification/main.yml",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
